a6d64549c5681272019fb952d3f4eb2e1eed8d88
FOZLAUR <fozla@oocl.com>
2020-10-13 22:23:26 +0800

    [POS Machine Practic][Laurence-1] feature : 1. added the full database as seen in the instructions to ItemDataLoader.java.; 2. added the functions as seen in context-map.png in PosMachine.java.; 3. added files: PDCA.md and context-map.png

---
+++ PDCA.md
@@ -0,0 +1,31 @@
+**FOZLAUR/pos-machine-practice-2020-10-11-13-53-6-305
+
+getAllItemsFromDB
+P - 1 min
+D - 10 min
+C - Completed within alloted time; DB instantiation in ItemDataLoader was successful
+A - N/A
+
+getQuantitiesOfItems
+P - 2 min
+D - 10 min
+C - Confused array and ArrayList and List functionalities; Went beyond by 5 mins
+A - studied difference between each datatype and their different functionalities
+
+printItemDetails
+P - 3 min
+D - 15 min
+C - This task was a big umbrella function that includes the two following functions: calculateSubtotal and calculateTotal
+A - Should allot more time to these bigger tasks over the smaller or split tasks
+
+calculateSubtotal
+P - 2 min
+D - 15 min
+C - Completed within alloted time
+A - In relation to printItemDetails, shorten time for split tasks
+
+calculateTotal
+P - 2 min
+D - 15 min
+C - Completed within alloted time
+A - In relation to printItemDetails, shorten time for split tasks
--- src/main/java/pos/machine/PosMachine.java
+++ src/main/java/pos/machine/PosMachine.java
@@ -1,9 +1,69 @@
 package pos.machine;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PosMachine {
     public String printReceipt(List<String> barcodes) {
-        return null;
+        List<ItemInfo> itemInfos = getAllItemsFromDB();
+        int quantities[] = getQuantitiesOfItems(itemInfos, barcodes);
+        String receipt = printItemDetails(itemInfos, quantities);
+        return receipt;
+    }
+
+    public List<ItemInfo> getAllItemsFromDB(){
+        return ItemDataLoader.loadAllItemInfos();
+    }
+
+    private int[] getQuantitiesOfItems(List<ItemInfo> itemInfos, List<String> barcodes) {
+        int itemQuantities[] = new int[itemInfos.size()];
+        for(int i=0; i<itemQuantities.length; i++){
+            itemQuantities[i] = 0;
+        }
+        for(int j=0; j<barcodes.size(); j++){
+            for(int k=0; k<itemInfos.size(); k++){
+                if(barcodes.get(j).equals(itemInfos.get(k).getBarcode())){
+                    itemQuantities[k] += 1;
+                }
+            }
+        }
+
+        return itemQuantities;
+    }
+
+    private String printItemDetails(List<ItemInfo> itemInfos, int[] quantities) {
+        String receipt = "***<store earning no money>Receipt***\n";
+        List<Integer> subtotals = new ArrayList<Integer>();
+
+        for(int i=0; i<itemInfos.size(); i++){
+            if(quantities[i] > 0){
+                receipt += ("Name: "+itemInfos.get(i).getName()+", ");
+                receipt += ("Quantity: "+quantities[i]+", ");
+                receipt += ("Unit price: "+itemInfos.get(i).getPrice()+" (yuan), ");
+                int subtotal = calculateSubtotal(itemInfos.get(i).getPrice(), quantities[i]);
+                receipt += ("Subtotal: "+subtotal+" (yuan)\n");
+                subtotals.add(subtotal);
+            }
+        }
+
+        if(subtotals.size() > 0){
+            receipt += "----------------------\n";
+            receipt += ("Total: "+calculateTotal(subtotals)+" (yuan)\n");
+            receipt += "**********************";
+        }
+
+        return receipt.equals("***<store earning no money>Receipt***\n") ? null : receipt;
+    }
+
+    private int calculateSubtotal(int price, int quantity) {
+        return price * quantity;
+    }
+
+    private int calculateTotal(List<Integer> subtotals) {
+        int total = 0;
+        for(int subtotal : subtotals){
+            total += subtotal;
+        }
+        return total;
     }
 }
